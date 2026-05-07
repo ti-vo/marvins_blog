6760eac7a9d6e677e4c03eb611b3f856a3ae069f
ti-vo <teresa.vogl@uni-leipzig.de>
2026-05-07 13:13:15 +0200

    migrated from function-based views to class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="landing_page"),
-    path("posts", views.posts, name="posts_page"),
-    path("posts/<slug:slug>", views.single_post, name="post-detail-page")  # posts/my-first-post is search engine friendly
+    path("", views.StartingPageView.as_view(), name="landing_page"),
+    path("posts", views.AllPostsView.as_view(), name="posts_page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")  # posts/my-first-post is search engine friendly
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,63 @@
 from django.shortcuts import render, get_object_or_404
+from typing import List
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 
 # Create your views here.
 
+# class-based view
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts" # the name to be passed on to template
+
+    def get_queryset(self):
+        """overwrite internal function to limit to three posts"""
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# function based view, obsolete:
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3] # Django behind the scenes only fetches 3 database entries. Does not support negative indexes
     #sorted_posts = sorted(all_posts, key=get_date)
     #latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {"posts": latest_posts})
+    return render(request, "blog/index.html", 
+                  {"posts": latest_posts} #pass posts to template
+                  )
 
+
+
+class AllPostsView(ListView):
+    template_name = "blog/all-post.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# function based view, obsolete
 def posts(request):
     all_posts = Post.objects.all().order_by("-date") # -date descending order
     return render(request, "blog/all-posts.html", {
        "all_posts": all_posts
     }) 
 
+# class based view
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+    # will automatically search by slug!
+
+    def get_context_data(self, **kwargs):
+        """overwrite to show tags""" 
+        context_data = super().get_context_data(**kwargs)
+        context_data["post_tags"] = self.object.tags.all()
+        return context_data
+       
+
+# obsolete function based view:
 def single_post(request, slug):
 
     identified_post = get_object_or_404(Post, slug=slug)
